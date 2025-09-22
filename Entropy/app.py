2c9b528e5f83457b1d2a51ca3378580a540547d0
Dimoscore <mathgascon@laposte.net>
2025-09-22 16:05:15 +0200

    Nouvelle version pour Render

--- Entropy/app.py
+++ Entropy/app.py
@@ -1,6 +1,9 @@
-﻿from Class.CConfig import Config
+﻿import os
+
+from Class.CConfig import Config
 from Controller.ApiUser import ApiUser
 from Api import init_api  # ton API Swagger
+from flask import Flask, render_template
 from threading import Thread
 
 # Initialisation de la config et de l'app
@@ -9,7 +12,14 @@ app = conf.app
 
 # Enregistrement du blueprint web
 app.register_blueprint(ApiUser)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(BASE_DIR, "templates"),
+        static_folder=os.path.join(BASE_DIR, "static")
+    )
 # Initialisation de l'API Swagger sur le même app mais sous /Api
 api = init_api(app, prefix='/Api')  # on passe le préfixe pour Swagger
 
