60aa30f0458adb76ddf18ce250d7b0241d3a6643
Dimoscore <mathgascon@laposte.net>
2025-09-23 00:25:30 +0200

    Nouvelle version pour Render

--- Entropy/Class/CConfig.py
+++ Entropy/Class/CConfig.py
@@ -1,25 +1,18 @@
 ﻿import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Config:
-    """
-    Configuration Flask et SQLAlchemy
-    """
-    # Lecture des variables d'environnement Render
-    POSTGRES_USER = os.environ.get("POSTGRES_USER", "user")
-    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "password")
-    POSTGRES_DB = os.environ.get("POSTGRES_DB", "db")
-    POSTGRES_SERVER = os.environ.get("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "5432")
+    def __init__(self):
+        # Crée l'application Flask
+        self.app = Flask(__name__, template_folder="Entropy/templates", static_folder="Entropy/static")
 
-    SECRET_KEY = os.environ.get("SECRET_KEY", "ma_cle_secrete_par_defaut")
+        # Configuration Flask
+        self.app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "une_cle_par_defaut")
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("POSTGRES_SERVER")
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Construction de l'URI SQLAlchemy correctement
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-        f"@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+        # Initialisation SQLAlchemy
+        db.init_app(self.app)
